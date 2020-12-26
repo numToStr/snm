b74c554c8c8c5e9ac9f6f83f481c370ad8a218c3
numtostr <vikasraj1911@gmail.com>
2020-12-26 15:21:29 +0530

    prevent lts/* and alias in `snm install`

--- src/cmd/install.rs
+++ src/cmd/install.rs
@@ -1,7 +1,7 @@
 use crate::config::Config;
 use crate::downloader::Downloader;
 use crate::fetcher::Fetcher;
-use crate::version::Version;
+use crate::version::{NodeVersion, Version};
 use clap::Clap;
 
 #[derive(Debug, Clap, PartialEq, Eq)]
@@ -14,13 +14,23 @@ impl super::Command for Install {
     type InitResult = ();
 
     fn init(&self, config: Config) -> anyhow::Result<Self::InitResult> {
-        let release = Fetcher::fetch(&config.dist_mirror)?.find_release(&self.version);
+        let can_install = match &self.version {
+            Version::Full(NodeVersion::Lts(_)) => false,
+            Version::Full(NodeVersion::Alias(_)) => false,
+            _ => true,
+        };
 
-        match release {
-            Some(r) => {
-                Downloader.download(&r, &config)?;
+        if !can_install {
+            println!("Requested version ({}) is not installable", &self.version);
+        } else {
+            let release = Fetcher::fetch(&config.dist_mirror)?.find_release(&self.version);
+
+            match release {
+                Some(r) => {
+                    Downloader.download(&r, &config)?;
+                }
+                _ => println!("No release found with the version {}", &self.version),
             }
-            _ => println!("No release found with the version {}", &self.version),
         }
 
         Ok(())
