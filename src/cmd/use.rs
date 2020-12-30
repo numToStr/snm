072a028a3a8e2d0139b348359cfd46a316a2fc6b
numtostr <vikasraj1911@gmail.com>
2020-12-30 17:06:55 +0530

    allow <alias> and lts* in `snm use`
    Fixes #10

--- src/cmd/use.rs
+++ src/cmd/use.rs
@@ -14,14 +14,31 @@ impl super::Command for Use {
     type InitResult = ();
 
     fn init(&self, config: Config) -> anyhow::Result<Self::InitResult> {
-        let dir = config.release_dir();
-        let versions = NodeVersion::list_versions(&dir)?;
+        match &self.version {
+            // Get the link dest of the alias and symlink default to dest rather than symlinking to alias
+            // both Alias(_) and Lts() are treated as symlinks
+            Version::Full(NodeVersion::Lts(_)) | Version::Full(NodeVersion::Alias(_)) => {
+                let alias = &self.version.to_string();
+                let link = config.alias_dir().join(alias);
 
-        let version = self.version.to_node_version(&versions)?;
+                if link.exists() {
+                    let dest = std::fs::read_link(link)?;
 
-        symlink_to(dir.join(version.version_str()), config.alias_default())?;
+                    symlink_to(&dest, &config.alias_default())?;
 
-        println!("Using Node.js {}", version.version_str().bold());
+                    println!("Using Alias {}", &alias.bold());
+                }
+            }
+            _ => {
+                let dir = config.release_dir();
+                let versions = NodeVersion::list_versions(&dir)?;
+
+                let version = self.version.to_node_version(&versions)?;
+
+                symlink_to(dir.join(version.version_str()), config.alias_default())?;
+                println!("Using Node.js {}", version.version_str().bold());
+            }
+        };
 
         Ok(())
     }
