0bf195edcfcca07ea82d362a79f016124f6ddffb
numtostr <vikasraj1911@gmail.com>
2021-05-13 20:29:53 +0530

    new downloader

--- src/downloader.rs
+++ src/downloader.rs
@@ -1,11 +1,79 @@
-use crate::config::Config;
 use crate::fetcher::Release;
 use crate::symlink::symlink_to;
 use crate::url;
 use crate::{archive::Archive, progress_bar::Bar};
+use crate::{config::Config, url::Dist};
 use colored::*;
 use indicatif::HumanBytes;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+
+pub struct Downloader<'a> {
+    release: &'a Release,
+    config: &'a Config,
+    dist: Dist,
+}
+
+impl<'a> Downloader<'a> {
+    pub fn new(release: &'a Release, config: &'a Config) -> Self {
+        let dist = url::release(&config.dist_mirror, &release.version);
+
+        Self {
+            release,
+            config,
+            dist,
+        }
+    }
+
+    pub fn download(&self) -> anyhow::Result<Vec<u8>> {
+        let r = &self.release;
+        let res = ureq::get(&self.dist.url).call()?;
+        let len = res
+            .header("Content-Length")
+            .and_then(|x| x.parse::<u64>().ok());
+
+        let size = match len {
+            Some(l) => HumanBytes(l).to_string(),
+            None => "unknown".into(),
+        };
+
+        println!("Version   : {}", r.version.to_string().bold());
+        println!("Download  : {}", self.dist.url.bold());
+        println!("Size      : {}", size.bold());
+
+        println!();
+
+        let buf = Bar::new(len).read_start(res.into_reader())?;
+
+        println!();
+
+        Ok(buf)
+    }
+
+    pub fn install(&self, buf: Vec<u8>) -> anyhow::Result<PathBuf> {
+        let release_dir = self.config.release_dir();
+        let dest = release_dir.join(&self.release.version.to_string());
+
+        Archive::new(buf).extract_into(&release_dir)?;
+
+        std::fs::rename(&release_dir.join(&self.dist.name), &dest)?;
+
+        println!("Installed : {}", &dest.display().to_string().bold());
+
+        Ok(dest)
+    }
+
+    pub fn alias_to_default(&self, dest: &Path) -> anyhow::Result<()> {
+        symlink_to(&dest, &self.config.alias_default().as_path())?;
+
+        println!();
+        println!(
+            "-- Using version {} --",
+            self.release.version.to_string().bold()
+        );
+
+        Ok(())
+    }
+}
 
 pub fn download(r: &Release, config: &Config) -> anyhow::Result<PathBuf> {
     let release_dir = &config.release_dir();
