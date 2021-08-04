5f6872241e9f734c532221ce23fb16e9caeb0de9
numtostr <vikasraj1911@gmail.com>
2021-08-04 14:31:08 +0530

    wilcard semver

--- src/lib/version/mod.rs
+++ src/lib/version/mod.rs
@@ -3,7 +3,7 @@ use super::SnmRes;
 pub mod dist_version;
 pub mod user_version;
 
-pub trait ParseVersion<'a> {
+pub(super) trait ParseVersion<'a> {
     type Item;
     fn parse(v: &'a str) -> SnmRes<Self::Item>;
 }
--- src/lib/version/user_version.rs
+++ src/lib/version/user_version.rs
@@ -1,3 +1,4 @@
+use semver::VersionReq;
 use serde::Deserialize;
 use std::{env::current_dir, fmt::Display, fs::read_to_string, str::FromStr};
 
@@ -9,7 +10,7 @@ use crate::lib::{
 use super::{dist_version::DistVersion, ParseVersion};
 
 const PACKAGE_JSON: &str = "package.json";
-const NODE_FILES: [&str; 3] = [".nvmrc", ".node-version", PACKAGE_JSON];
+const VERSION_FILES: [&str; 3] = [".nvmrc", ".node-version", PACKAGE_JSON];
 
 /// `UserVersion` represents the user provided version
 /// It could be alias, lts codename, partial or full semver
@@ -21,6 +22,8 @@ pub enum UserVersion {
     MajorMinor(u64, u64),
     /// Full semver ie. 14.17.8
     Full(DistVersion),
+    /// Wildcard semver ie. >14.14 <=12.3
+    WildCard(VersionReq),
     /// Alias name ie. latest, lts
     Alias(String),
     /// LTS codename ie. Fermium, Erbium
@@ -38,24 +41,31 @@ impl ParseVersion<'_> for UserVersion {
 
         let trimmed = ver.trim_start_matches('v');
 
-        // Check whether alias, if first char is not numeric
-        if let Some(ch) = trimmed.chars().next() {
-            if !ch.is_numeric() {
-                return Ok(Self::Alias(ver.to_string()));
-            }
-        };
-
+        // check if the version is a semver string
         let m = match DistVersion::parse(trimmed) {
             Ok(dist_version) => Self::Full(dist_version),
             Err(_) => {
-                let mut s = trimmed.splitn(2, '.');
+                // Check if the version is wildcard semver
+                match VersionReq::parse(ver) {
+                    Ok(x) => Self::WildCard(x),
+                    Err(err) => {
+                        // Check whether alias, if first char is not numeric
+                        if let Some(ch) = trimmed.chars().next() {
+                            if !ch.is_numeric() {
+                                return Ok(Self::Alias(ver.to_string()));
+                            }
+                        };
 
-                match (s.next(), s.next()) {
-                    (Some(maj), None) => Self::Major(maj.parse::<u64>()?),
-                    (Some(maj), Some(min)) => {
-                        Self::MajorMinor(maj.parse::<u64>()?, min.parse::<u64>()?)
+                        let mut s = trimmed.splitn(2, '.');
+
+                        match (s.next(), s.next()) {
+                            (Some(maj), None) => Self::Major(maj.parse::<u64>()?),
+                            (Some(maj), Some(min)) => {
+                                Self::MajorMinor(maj.parse::<u64>()?, min.parse::<u64>()?)
+                            }
+                            _ => anyhow::bail!(err),
+                        }
                     }
-                    _ => anyhow::bail!("WTF"),
                 }
             }
         };
@@ -77,9 +87,10 @@ impl Display for UserVersion {
         match self {
             Self::Major(x) => write!(f, "{}.x.x", x),
             Self::MajorMinor(x, y) => write!(f, "{}.{}.x", x, y),
-            Self::Full(x) => write!(f, "{}", x.to_string()),
-            Self::Lts(x) => write!(f, "lts-{}", x),
+            Self::Full(x) => x.fmt(f),
+            Self::WildCard(x) => x.fmt(f),
             Self::Alias(x) => f.write_str(x.as_str()),
+            Self::Lts(x) => write!(f, "lts-{}", x),
         }
     }
 }
@@ -98,11 +109,10 @@ impl UserVersion {
     pub fn match_release(&self, release: &Release) -> bool {
         match (self, &release.version, &release.lts) {
             (Self::Full(a), b, _) => a == b,
+            (Self::WildCard(a), DistVersion(b), _) => a.matches(b),
             (Self::MajorMinor(maj, min), DistVersion(x), _) => *maj == x.major && *min == x.minor,
             (Self::Major(a), DistVersion(b), _) => *a == b.major,
             (Self::Lts(a), _, Lts::Yes(b)) => a.to_lowercase() == b.to_lowercase(),
-            // FIXME: add wildcard semver
-            // (Self::Alias(_), _) => false,
             _ => false,
         }
     }
@@ -110,7 +120,7 @@ impl UserVersion {
     pub fn from_file() -> SnmRes<Self> {
         let pwd = current_dir()?;
 
-        for f_name in NODE_FILES {
+        for f_name in VERSION_FILES {
             let f_path = pwd.join(&f_name);
 
             if !f_path.exists() {
