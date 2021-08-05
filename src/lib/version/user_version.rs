d4635aa5c9fecd6395b3c715aac3ceaac9d5e763
numtostr <vikasraj1911@gmail.com>
2021-08-05 10:33:20 +0530

    remove Full variant from UserVersion, already covered by VersionReq

--- src/lib/version/user_version.rs
+++ src/lib/version/user_version.rs
@@ -16,44 +16,26 @@ const VERSION_FILES: [&str; 3] = [".nvmrc", ".node-version", PACKAGE_JSON];
 /// It could be alias, lts codename, partial or full semver
 #[derive(Debug, PartialEq, Eq)]
 pub enum UserVersion {
-    /// Full semver ie. 14.17.8
-    Full(DistVersion),
-    /// Partial or Range semver ie. 12 | 14.13 | >14.14 | <=12.3
-    Partial(VersionReq),
+    /// Full, Partial or Range semver ie. 14 | 14.17 | 14.17.4 | >14.14 | <=12.3
+    Semver(VersionReq),
     /// Alias name ie. latest, lts
     Alias(String),
-    /// LTS codename ie. Fermium, Erbium
-    /// NOTE: this should start with `lts/` or `lts-`
+    /// LTS codename ie. fermium, erbium
     Lts(String),
 }
 
 impl ParseVersion<'_> for UserVersion {
     type Item = Self;
     fn parse(ver: &str) -> SnmRes<Self::Item> {
-        // Check if Lts
-        if ver.starts_with("lts/") || ver.starts_with("lts-") {
-            return Ok(Self::Lts(ver[4..].to_string()));
-        }
-
-        let trimmed = ver.trim_start_matches('v');
-
         // check if the version is a semver string
-        let m = match DistVersion::parse(trimmed) {
-            Ok(dist_version) => Self::Full(dist_version),
+        let m = match VersionReq::parse(ver.trim_start_matches('v')) {
+            Ok(x) => Self::Semver(x),
             Err(_) => {
-                // Check if the version is wildcard semver
-                match VersionReq::parse(ver) {
-                    Ok(x) => Self::Partial(x),
-                    Err(err) => {
-                        // Check whether alias, if first char is not numeric
-                        if let Some(ch) = trimmed.chars().next() {
-                            if !ch.is_numeric() {
-                                return Ok(Self::Alias(ver.to_string()));
-                            }
-                        };
-
-                        anyhow::bail!(err)
-                    }
+                // Check if Lts, else alias
+                if ver.starts_with("lts/") || ver.starts_with("lts-") {
+                    Self::Lts(ver[4..].to_string())
+                } else {
+                    Self::Alias(ver.to_string())
                 }
             }
         };
@@ -62,7 +44,7 @@ impl ParseVersion<'_> for UserVersion {
     }
 }
 
-// NOTE: this is used by `clap`
+// NOTE: this is used by `clap` to parse use input
 impl FromStr for UserVersion {
     type Err = anyhow::Error;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
@@ -73,8 +55,7 @@ impl FromStr for UserVersion {
 impl Display for UserVersion {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Full(x) => x.fmt(f),
-            Self::Partial(x) => x.fmt(f),
+            Self::Semver(x) => x.fmt(f),
             Self::Alias(x) => f.write_str(x.as_str()),
             Self::Lts(x) => write!(f, "lts-{}", x),
         }
@@ -94,8 +75,7 @@ pub struct PkgJson {
 impl UserVersion {
     pub fn match_release(&self, release: &Release) -> bool {
         match (self, &release.version, &release.lts) {
-            (Self::Full(a), b, _) => a == b,
-            (Self::Partial(a), DistVersion(b), _) => a.matches(b),
+            (Self::Semver(a), DistVersion(b), _) => a.matches(b),
             (Self::Lts(a), _, Lts::Yes(b)) => a.to_lowercase() == b.to_lowercase(),
             _ => false,
         }
