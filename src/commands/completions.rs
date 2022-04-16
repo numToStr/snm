8df66b3a3546cb76f19b5211dc5556c31d2f8c2e
Eduardo Pinho <enet4mikeenet@gmail.com>
2022-04-16 14:25:03 +0100

    Update clap to 3.1
    - bump clap to 3.1.9
       - feature "derive" needed for `Parser` derive macro
       - feature "cargo" required for `crate_authors` and other macros
       - feature "env" required for `env` field in clap attribute
    - replace deprecated cargo_generate with cargo_complete
    - replace deprecated constructs

--- src/commands/completions.rs
+++ src/commands/completions.rs
@@ -1,6 +1,6 @@
 use crate::cli::{Cli, Config};
 use clap::{crate_name, IntoApp, Parser};
-use clap_generate::{generate, generators};
+use clap_complete::{generate, shells};
 use snm_core::{shell::ShellKind, SnmRes};
 
 #[derive(Debug, Parser)]
@@ -13,21 +13,21 @@ pub struct Completions {
 impl super::Command for Completions {
     fn init(self, _: Config) -> SnmRes<()> {
         let name = crate_name!();
-        let mut app = Cli::into_app();
+        let mut app = Cli::command();
         let mut stdout = std::io::stdout();
 
         match &self.shell {
             ShellKind::Bash => {
-                generate(generators::Bash, &mut app, name, &mut stdout);
+                generate(shells::Bash, &mut app, name, &mut stdout);
             }
             ShellKind::Zsh => {
-                generate(generators::Zsh, &mut app, name, &mut stdout);
+                generate(shells::Zsh, &mut app, name, &mut stdout);
             }
             ShellKind::Fish => {
-                generate(generators::Fish, &mut app, name, &mut stdout);
+                generate(shells::Fish, &mut app, name, &mut stdout);
             }
             ShellKind::Pwsh => {
-                generate(generators::PowerShell, &mut app, name, &mut stdout);
+                generate(shells::PowerShell, &mut app, name, &mut stdout);
             }
         };
 
